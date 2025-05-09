4f3ed9600141fbd9f9e2ac3636df8dd16239dec7
dharmik.jasoliya@tatvasoft.com <dharmik.jasoliya@tatvasoft.com>
2025-05-09 18:30:09 +0530

    calender ui and api integration

---
+++ src/app/user/my-calender/CalenderDropdown.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from '@/components/ui/select';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+interface Props {
+  title: string;
+  allowedTypes: string[];
+  handleNext: () => void;
+  handlePrev: () => void;
+  onChange: (filter: string) => void;
+}
+
+export default function CalenderDropdown({
+  title,
+  onChange,
+  handleNext,
+  handlePrev,
+  allowedTypes,
+}: Readonly<Props>) {
+  const [value, setValue] = useState("dayGridMonth");
+
+  useEffect(() => {
+    onChange("dayGridMonth");
+  }, []);
+
+  const onValueChange = (value: string) => {
+    setValue(value);
+    onChange(value);
+  }
+
+  return (
+    <div className='flex justify-between items-center mb-5'>
+      <p className="text-sm mb-1 font-bold">
+        <span className="text-[14px] text-black md:text-[22px]">{title}</span>
+      </p>
+      <div className="flex text-black gap-2 items-center">
+        {allowedTypes.length > 0 && (
+          <Select
+            value={value}
+            onValueChange={onValueChange}
+            disabled={allowedTypes.length === 1}
+          >
+            <SelectTrigger className="w-[150px] focus:outline-none focus:ring-0 focus:ring-offset-0 border-gray-300">
+              <SelectValue placeholder="Select Range" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="today">Today</SelectItem>
+              <SelectItem value="listWeek">List View</SelectItem>
+              <SelectItem value="timeGridWeek">Week View</SelectItem>
+              <SelectItem value="dayGridMonth">Month View</SelectItem>
+            </SelectContent>
+          </Select>
+        )}
+        <div className="flex items-center gap-2">
+          <Button
+            size="sm"
+            title="Previous"
+            className="bg-black text-white hover:bg-black/90 disabled:opacity-50 h-8 w-8 p-0 cursor-pointer disabled:cursor-not-allowed"
+            onClick={handlePrev}
+          >
+            <ChevronLeft className="h-4 w-4 text-white" />
+          </Button>
+          <Button
+            size="sm"
+            title="Next"
+            className="bg-black text-white hover:bg-black/90 disabled:opacity-50 h-8 w-8 p-0 cursor-pointer disabled:cursor-not-allowed"
+            onClick={handleNext}
+          >
+            <ChevronRight className="h-4 w-4 text-white" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/user/my-calender/helper.ts
@@ -0,0 +1,56 @@
+export const eventStyles: Record<string, {}> = {
+  "Music": {
+    name: "Light Blue",
+    backgroundColor: "#e0f2fe",
+    borderColor: "#bae6fd",
+    textColor: "#1d4ed8",
+  },
+  "Art & Culture": {
+    name: "Soft Purple",
+    backgroundColor: "#ede9fe",
+    borderColor: "#ddd6fe",
+    textColor: "#7c3aed",
+  },
+  "Film & Media": {
+    name: "Pink",
+    backgroundColor: "#fce7f3",
+    borderColor: "#fbcfe8",
+    textColor: "#db2777",
+  },
+  "Education": {
+    name: "Orange",
+    backgroundColor: "#fff7ed",
+    borderColor: "#fdba74",
+    textColor: "#ea580c",
+  },
+  "Sports": {
+    name: "Green",
+    backgroundColor: "#ecfdf5",
+    borderColor: "#6ee7b7",
+    textColor: "#059669",
+  },
+  "Food & Drink": {
+    name: "Slate/Gray",
+    backgroundColor: "#f1f5f9",
+    borderColor: "#cbd5e1",
+    textColor: "#334155",
+  },
+  "Wellness": {
+    name: "Yellow",
+    backgroundColor: "#fef9c3",
+    borderColor: "#fde68a",
+    textColor: "#b45309",
+  },
+  "Gaming": {
+    name: "Sky Blue",
+    backgroundColor: "#e0f2fe",
+    borderColor: "#7dd3fc",
+    textColor: "#0284c7",
+  },
+  "Business": {
+    name: "Red",
+    backgroundColor: "#fee2e2",
+    borderColor: "#fca5a5",
+    textColor: "#b91c1c",
+  }
+};
