11b444c4d387490f262cfbfdb743d0c0400caa5e
dharmik.jasoliya@tatvasoft.com <dharmik.jasoliya@tatvasoft.com>
2025-05-30 21:55:07 +0530

    typing msg, chat pagination and leave group changes

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState, useRef } from 'react';
 import moment from 'moment';
 import { SendHorizonalIcon, InfoIcon, Trash2, LogOut, XIcon } from "lucide-react";
+import { toast } from 'react-toastify';
 
 import { connectSocket, disconnectSocket, getSocket } from './socket';
 import { apiCall } from '@/utils/services/request';
@@ -11,36 +12,18 @@ import { IGroup, IMessage } from './type';
 const Chat = () => {
   const [userId, setUserId] = useState<null | string>(null);
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [groupedMessage, setGroupedMessage] = useState<Record<string, IMessage[]>>({});
   const [newMessage, setNewMessage] = useState<string>("");
   const [myGroups, setMyGroups] = useState<IGroup[]>([]);
   const [activeGroup, setActiveGroup] = useState<string | null>(null);
   const [openGroupInfo, setOpenGroupInfo] = useState(false);
-  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const [hasMoreMessages, setHasMoreMessages] = useState(true);
+  const [loadingOlderMessages, setLoadingOlderMessages] = useState(false);
+  const [isScrollablePrevent, setIsScrollablePrevent] = useState(false);
+  const [typingUsers, setTypingUsers] = useState<string[]>([]);
 
-  const fetchMyGroup = async () => {
-    try {
-      const response = await apiCall({
-        endPoint: '/chat/my-group',
-        method: "GET",
-        withToken: true,
-      });
-      if (response.data) {
-        setMyGroups(response.data.map((group: any) => ({
-          id: group.id,
-          name: group.name,
-          icon: group.icon,
-          members: group.members,
-          senderId: group.senderId || null,
-          lastMessage: group.lastMessage?.content ?? '',
-          lastMessageSender: group.lastMessage?.sender.name || '',
-          lastMessageTime: group.lastMessage ? moment(group.lastMessage.createdAt).format('hh:mm A') : '',
-        })));
-        setUserId(response.userId);
-      }
-    } catch (error) {
-      console.error("Failed to fetch groups:", error);
-    }
-  };
+  const typingTimeoutRef = useRef<any>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     connectSocket();
@@ -58,9 +41,11 @@ const Chat = () => {
 
     socket.on("initial_messages", (data) => {
       setMessages(data.messages);
+      setGroupedMessage(groupMessagesByDate(data.messages));
+      setIsScrollablePrevent(false);
     });
 
-    socket.on('group_member_updated', ({ groupId, newMember }) => {
+    socket.on('group_member_added', ({ groupId, newMember }) => {
       setMyGroups(prev => prev.map((group) =>
         group.id === groupId
           ? {
@@ -78,9 +63,34 @@ const Chat = () => {
       ));
     });
 
+    socket.on('group_member_removed', ({ groupId, removedMemberId }) => {
+      if (removedMemberId === userId) {
+        setMessages([]);
+        setActiveGroup(null);
+        setGroupedMessage({});
+        setOpenGroupInfo(false);
+        setIsScrollablePrevent(false);
+        setHasMoreMessages(false);
+        setLoadingOlderMessages(false);
+        setMyGroups(prev => prev.filter(group => group.id !== groupId));
+        toast.success("You removed from the group chat!");
+      } else {
+        setMyGroups(prev => prev.map((group) =>
+          group.id === groupId
+            ? {
+              ...group,
+              members: group.members.filter(member => member.id !== removedMemberId)
+            }
+            : group
+        ));
+      }
+    });
+
     socket.on("new_group_message", (message) => {
       if (message.group === activeGroup) {
         setMessages(prev => [...prev, message]);
+        setGroupedMessage(groupMessagesByDate([...messages, message]));
+        setIsScrollablePrevent(false);
         setMyGroups(prev => prev.map((group) =>
           group.id === message.group
             ? {
@@ -96,18 +106,98 @@ const Chat = () => {
     });
 
     return () => {
-      socket.emit("leave_group_chat", { groupId: activeGroup });
       socket.off("initial_messages");
       socket.off("new_group_message");
+      socket.off("group_member_added");
+      socket.off("group_member_removed");
     };
   }, [activeGroup]);
 
   useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
+    const socket = getSocket();
+    if (!activeGroup || !socket) return;
+
+    socket.on('user_typing', ({ user, groupId: typingGroupId }) => {
+      if (typingGroupId === activeGroup && user._id !== userId) {
+        setTypingUsers((prev) => [...new Set([...prev, user])]);
+      }
+    });
+
+    socket.on('user_stopped_typing', ({ user, groupId: typingGroupId }) => {
+      if (typingGroupId === activeGroup) {
+        setTypingUsers((prev) => prev.filter((name) => name !== user));
+      }
+    });
+
+    return () => {
+      socket.off('user_typing');
+      socket.off('user_stopped_typing');
+    };
+  }, [activeGroup]);
+
+  useEffect(() => {
+    !isScrollablePrevent && scrollToBottom();
+  }, [messages, isScrollablePrevent]);
+
+  const fetchMyGroup = async () => {
+    try {
+      const response = await apiCall({
+        endPoint: '/chat/my-group',
+        method: "GET",
+        withToken: true,
+      });
+      if (response.data) {
+        setMyGroups(response.data.map((group: any) => ({
+          id: group.id,
+          name: group.name,
+          icon: group.icon,
+          members: group.members,
+          senderId: group.senderId || null,
+          lastMessage: group.lastMessage,
+          lastMessageSender: group.lastMessageSender,
+          lastMessageTime: group.lastMessage ? moment(group.lastMessageTime).format('hh:mm A') : '',
+        })));
+        setUserId(response.userId);
+      }
+    } catch (error) {
+      console.error("Failed to fetch groups:", error);
+    }
+  };
+
+  const loadOlderMessages = async () => {
+    if (loadingOlderMessages || !hasMoreMessages || messages.length === 0) return;
+
+    setLoadingOlderMessages(true);
+    try {
+      const oldestMessageDate = messages[0].createdAt;
+      const response = await apiCall({
+        endPoint: `/chat/messages/${activeGroup}?before=${oldestMessageDate}`,
+        method: "GET",
+        withToken: true,
+      });
+
+      if (response.data && response.data.length > 0) {
+        setMessages(prev => [...response.data, ...prev]);
+        setGroupedMessage(groupMessagesByDate([...response.data, ...messages]));
+        setIsScrollablePrevent(true);
+        setHasMoreMessages(response.hasMore);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setLoadingOlderMessages(false);
+    }
+  };
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const element = e.currentTarget;
+    if (element.scrollTop === 0 && hasMoreMessages) {
+      loadOlderMessages();
+    }
   };
 
   const groupMessagesByDate = (messages: IMessage[]) => {
@@ -132,6 +222,21 @@ const Chat = () => {
     }, {});
   };
 
+  const handleTyping = () => {
+    const socket = getSocket();
+    socket.emit('typing', { groupId: activeGroup });
+
+    clearTimeout(typingTimeoutRef?.current);
+    typingTimeoutRef.current = setTimeout(() => {
+      socket.emit('stop_typing', { groupId: activeGroup });
+    }, 1000);
+  };
+
+  const onInputChange = (value: string) => {
+    setNewMessage(value);
+    handleTyping();
+  };
+
   const sendMessage = () => {
     if (!newMessage.trim() || !activeGroup) return;
 
@@ -143,6 +248,15 @@ const Chat = () => {
 
     setNewMessage("");
   };
+
+  const leaveGroupChat = async () => {
+    if (!activeGroup) return;
+
+    const socket = getSocket();
+    socket.emit("leave_group_chat", {
+      groupId: activeGroup,
+    });
+  }
 
   return (
     <div className="w-full flex bg-gray-100">
@@ -177,13 +291,13 @@ const Chat = () => {
             </button>
           ))}
         </div>
-        <div className="flex justify-around p-3 border-t bg-white">
-          <button className="text-gray-500 cursor-pointer">
+        <div className="flex justify-around border-t bg-white">
+          <button className="p-3 w-1/2 text-gray-500 cursor-pointer">
             Users
           </button>
           <button
             onClick={() => fetchMyGroup()}
-            className="text-gray-500 cursor-pointer"
+            className="p-3 w-1/2 text-gray-500 cursor-pointer"
           >
             Groups
           </button>
@@ -216,13 +330,21 @@ const Chat = () => {
                 <button className='cursor-pointer' onClick={() => setOpenGroupInfo(true)}><InfoIcon /></button>
               </div>
             </div>
-            <div className="flex-1 overflow-y-auto px-6 py-4 space-y-3 bg-[#f5f5f5]">
+            <div
+              className="flex-1 overflow-y-auto px-6 py-4 space-y-3 bg-[#f5f5f5]"
+              onScroll={handleScroll}
+            >
+              {loadingOlderMessages && (
+                <div className="flex justify-center py-2">
+                  <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-purple-500"></div>
+                </div>
+              )}
               {messages.length === 0 && (
                 <div className="text-center text-gray-500">
                   No messages yet. Start chatting!
                 </div>
               )}
-              {Object.entries(groupMessagesByDate(messages)).map(([dateTitle, msgs]) => (
+              {Object.entries(groupedMessage).map(([dateTitle, msgs]) => (
                 <div key={dateTitle}>
                   <div className='flex justify-center sticky top-0 mb-3'>
                     <div className='w-fit pt-[2px] pb-[2px] pl-2 pr-2 font-medium border rounded-[3px] bg-white'>
@@ -258,7 +380,7 @@ const Chat = () => {
                           {!isSentByMe && isFirstOfSequence && (
                             <div className="mb-[3px] text-sm font-semibold text-purple-500">{msg.sender.name}</div>
                           )}
-                          <div className={`min-w-[80px] max-w-xs rounded-xl p-3 pb-1 ${isSentByMe ? 'bg-purple-500 text-white' : 'bg-gray-200 text-gray-700'}`}>
+                          <div className={`min-w-[80px] max-w-xs rounded-lg p-3 pt-[9px] pb-1 ${isSentByMe ? 'bg-purple-500 text-white' : 'bg-gray-200 text-gray-700'}`}>
                             {msg.content}
                             <div className="mt-[5px] text-[10px] text-right">
                               {moment(msg.createdAt).format('hh:mm A')}
@@ -272,27 +394,37 @@ const Chat = () => {
               ))}
               <div ref={messagesEndRef} />
             </div>
-            <div className="border-t bg-white p-4 flex items-center gap-3">
-              <button>➕</button>
-              <input
-                type="text"
-                value={newMessage}
-                placeholder="Type a message"
-                onChange={(e) => setNewMessage(e.target.value)}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter' && newMessage.trim()) {
-                    sendMessage();
-                  }
-                }}
-                className="flex-1 p-2 border rounded-full bg-gray-200 outline-none"
-              />
-              <button
-                disabled={!newMessage.trim()}
-                onClick={() => sendMessage()}
-                className="text-purple-600 text-lg cursor-pointer hover:text-purple-800 disabled:opacity-50 disabled:cursor-not-allowed"
-              >
-                <SendHorizonalIcon />
-              </button>
+            <div className='flex justify-center mb-4'>
+              <div className="w-full ml-6 mr-6 bg-white rounded-md shadow-sm relative">
+                {typingUsers.length > 0 && (
+                  <div className="absolute top-[-26px] left-0 pl-[7px] pr-[5px] pt-[2px] pb-[4px] border-b border-b-gray-100 rounded-t-md bg-white">
+                    {typingUsers.join(', ')} {typingUsers.length > 1 ? 'are' : 'is'} typing...
+                  </div>
+                )}
+                <div className="flex items-center px-4 h-[45px] border-b border-b-gray-200">
+                  <input
+                    type="text"
+                    value={newMessage}
+                    placeholder="Enter your message here"
+                    onChange={(e) => onInputChange(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter' && newMessage.trim()) {
+                        sendMessage();
+                      }
+                    }}
+                    className="flex-1 border-none outline-none placeholder-gray-400 text-gray-700"
+                  />
+                </div>
+                <div className="p-2 px-2 flex items-center justify-end">
+                  <button
+                    disabled={!newMessage.trim()}
+                    onClick={() => sendMessage()}
+                    className="text-purple-600 text-lg cursor-pointer hover:text-purple-800 disabled:opacity-50 disabled:cursor-not-allowed"
+                  >
+                    <SendHorizonalIcon />
+                  </button>
+                </div>
+              </div>
             </div>
           </>
         ) : (
@@ -328,7 +460,12 @@ const Chat = () => {
             </div>
             <div className="border-b border-b-gray-200 flex flex-col gap-4 p-4">
               <button className="flex justify-start items-center font-semibold text-[16px] text-[#f44649] cursor-pointer"><Trash2 className="mr-2 h-4 w-4" /> Delete Chat</button>
-              <button className="flex justify-start items-center font-semibold text-[16px] text-[#f44649] cursor-pointer"><LogOut className="mr-2 h-4 w-4" /> Leave</button>
+              <button
+                onClick={() => leaveGroupChat()}
+                className="flex justify-start items-center font-semibold text-[16px] text-[#f44649] cursor-pointer"
+              >
+                <LogOut className="mr-2 h-4 w-4" /> Leave
+              </button>
             </div>
             <div className="p-4">
               <div className="space-y-3">
